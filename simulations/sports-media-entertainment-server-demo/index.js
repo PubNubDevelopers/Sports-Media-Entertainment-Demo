35e4eeae0ad2d8c5c13b825669d651f658a0734f
markus.kohler <markus.kohler@pubnub.com>
2024-11-12 12:25:34 -0500

    disable old keyset

--- simulations/sports-media-entertainment-server-demo/index.js
+++ simulations/sports-media-entertainment-server-demo/index.js
@@ -4,9 +4,9 @@ const { createChannel } = require('../utils'); // assuming createChannel is in u
 
 const main = async () => {
   const chat = await Chat.init({
-    publishKey: 'pub-c-88b8ad7c-3270-45f8-97ce-f82a451b58ac',
-    subscribeKey: 'sub-c-722c2adb-390f-4153-849a-fa5700e5eaf2',
-    secretKey: 'sec-c-OGJhOGRhOGMtNGY4NC00MTM0LThhZjEtYWY3ZTg2MDU3M2U3',
+    publishKey: process.env.SPORTS_PUBNUB_PUBLISH_KEY,  // Loaded from .env
+    subscribeKey: process.env.SPORTS_PUBNUB_SUBSCRIBE_KEY,  // Loaded from .env
+    secretKey: process.env.SPORTS_PUBNUB_SECRET_KEY,  // Loaded from .env
     userId: "SIM"
   });
 
